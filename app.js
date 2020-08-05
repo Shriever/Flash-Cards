4ce77bce1c09e1cc8a080f34f950a1e5d5badac4
levi Lieberman <greggreg942@gmail.com>
2020-08-05 07:29:26 -0400

    Set up HTML/CSS and JS comments

---
+++ app.js
@@ -0,0 +1,13 @@
+// Question class: Represents a Question
+
+// Store Class: Stores the Questions in LocalStorage
+
+// UI Class: Deals with UI Control
+
+// Event: Display Questions
+
+// Event: Add Question
+
+// Event: Delete Question
+
+// Event: Edit Question
